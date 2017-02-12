190c0d62cf7b5fa758749931658210ae71b50706
argentumlupus <volchenok932@gmail.com>
2017-02-12 20:58:53 +0300

    bye-to-hello

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -8,6 +8,6 @@ for (var i = 0; i < names.length; i++) {
     console.log('Goodbye ' + names[i]);
     
   } else { 
-    console.log('Bye ' + names[i]);
+    console.log('Hello ' + names[i]);
   }
 }
