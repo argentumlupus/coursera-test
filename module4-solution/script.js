5469c8b5d2a630732be32ca4e291bb2f87512e2c
argentumlupus <volchenok932@gmail.com>
2017-02-12 21:11:40 +0300

    Get objects in scripts.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,13 +1,25 @@
+var speak = function(name) {
+  console.log(this.speakWord + ' ' + name);
+}
+
+var helloSpeaker = {
+  speakWord: 'Hello',
+  speak: speak
+}
+
+var byeSpeaker = {
+  speakWord: 'Goodbye',
+  speak: speak
+}
+
 
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
-
-  if ((names[i][0] == "J") || (names[i][0] == "j")) {
-    console.log('Goodbye ' + names[i]);
-    
-  } else { 
-    console.log('Hello ' + names[i]);
-  }
+  if (names[i].length)
+    if ((names[i][0] == "J") || (names[i][0] == "j"))
+      byeSpeaker.speak(names[i]);
+    else 
+      helloSpeaker.speak(names[i]);
 }
